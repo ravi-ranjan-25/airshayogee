ea49356f7b054c5d7ba00b3ddecb681628d31c57
Your Name <raviranjanv25@gmail.com>
2020-02-05 17:23:54 +0530

    ini

--- cab/admin.py
+++ cab/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from cab.models import userMob,Tax,wallet,cabDetail,cabOrder
+from cab.models import userMob,Tax,wallet,cabDetail,CabOrder
 # Register your models here.
 admin.site.register(userMob)
 admin.site.register(Tax)
 admin.site.register(wallet)
 admin.site.register(cabDetail)
-admin.site.register(cabOrder)
+admin.site.register(CabOrder)
 
 
